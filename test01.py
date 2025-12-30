8bfd378c18bb522311b309f65ecc54fe9d644021
stephangod <stephangod@email.com>
2025-12-30 12:21:13 +0900

    modified

--- test01.py
+++ test01.py
@@ -2,13 +2,13 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# print("np.__version__", np.__verseion__)
-# print("pd.__version__", pd.__verseion__)
-# print("OK")
+print("np.__version__", np.__version__)
+print("pd.__version__", pd.__version__)
+print("OK")
 
-num_list = [1,2,3,4,5]
-print(num_list)
-data = np.array(num_list)
-print(data)
-print(data.mean())
-print(data.max())
+# num_list = [1,2,3,4,5]
+# print(num_list)
+# data = np.array(num_list)
+# print(data)
+# print(data.mean())
+# print(data.max())
